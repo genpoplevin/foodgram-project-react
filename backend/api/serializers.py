e766cf69eebe15860e2615041e3c7f6ae14dd93c
Геннадий Поплевин <79530088804@yandex,ru>
2022-08-06 18:42:20 +0500

    Работа над изменением рецепта

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -63,7 +63,6 @@ class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(read_only=True, many=True)
     author = UserSerializer(read_only=True)
     ingredients = IngredientsInRecipeSerializer(
-        source='ingredientsinrecipe',
         many=True,
         read_only=True
     )
