5c5e6f184b621de3dbb1bea47dcf212653fb257b
Геннадий Поплевин <79530088804@yandex,ru>
2022-08-06 11:44:40 +0500

    add django_filter2

--- backend/api/utils/filters.py
+++ backend/api/utils/filters.py
@@ -1,7 +1,6 @@
-from django_filters.rest_framework import (
-    BooleanFilter, CharFilter, FilterSet, ModelMultipleChoiceFilter,
-    ModelChoiceFilter,
-)
+from django_filters.rest_framework import (BooleanFilter, CharFilter,
+                                           FilterSet, ModelChoiceFilter,
+                                           ModelMultipleChoiceFilter)
 from recipes.models import Ingredient, Recipe, Tag
 from users.models import User
 
