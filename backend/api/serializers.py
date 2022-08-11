bee551b6418bd07defba1e95835b590a1c5901b5
Геннадий Поплевин <79530088804@yandex,ru>
2022-08-11 22:27:54 +0500

    Получение рецептов в SubscribeSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -189,7 +189,7 @@ class SubscribeSerializer(UserSerializer):
     first_name = serializers.ReadOnlyField(source='author.first_name')
     last_name = serializers.ReadOnlyField(source='author.last_name')
     is_subscribed = serializers.SerializerMethodField()
-    recipes = serializers.SerializerMethodField()
+    recipes = RecipeSubscribesSerializer(many=True, source='author.recipes')
     recipes_count = serializers.SerializerMethodField()
 
     class Meta:
@@ -201,10 +201,6 @@ class SubscribeSerializer(UserSerializer):
         return Subscribe.objects.filter(
             user=obj.user, author=obj.author
         ).exists()
-
-    def get_recipes(self, obj):
-        queryset = Recipe.objects.filter(author=obj.author)
-        return RecipeSubscribesSerializer(queryset, many=True).data
 
     def get_recipes_count(self, obj):
         return Recipe.objects.filter(author=obj.author).count()
