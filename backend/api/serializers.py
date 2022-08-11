4d4975a1f8000ebe6655cfcfd8dcd176bfe85c19
Геннадий Поплевин <79530088804@yandex,ru>
2022-08-11 15:34:24 +0500

    Начал сначала работу над валидацией рецепта в сериализаторах2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -94,7 +94,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         ).exists()
 
     def validate(self, data):
-        ingredients = data.get('ingredientsinrecipe_set')
+        ingredients = data.get('ingredients')
         if not ingredients:
             raise serializers.ValidationError(
                 {'errors': 'Добавьте хотя бы один ингридиент в рецепт'})
@@ -115,7 +115,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
-        ingredients = validated_data.pop('ingredientsinrecipe_set')
+        ingredients = validated_data.pop('ingredients')
         recipe = Recipe.objects.create(**validated_data)
         for ingredient in ingredients:
             current_ingredient = ingredient['ingredient']
